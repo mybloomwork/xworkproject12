e61da354b6ed123cadc409b72553ee56090c9c82
mybloomwork <68631267+mybloomwork@users.noreply.github.com>
2020-09-25 10:51:24 +0100

    Update views.py

--- apps/store/views.py
+++ apps/store/views.py
@@ -37,8 +37,8 @@ def product_detail(request, category_slug, slug):
 
     related_products = list(product.category.products.filter(parent=None).exclude(id=product.id))
 
-    if len(related_products) >= 50:
-        related_products = random.sample(related_products, 50)
+    if len(related_products) >= 4:
+        related_products = random.sample(related_products, 4)
 
     if product.parent:
         return redirect('product_detail', category_slug=category_slug, slug=product.parent.slug)
